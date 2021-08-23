4971dfbff99e49df3b2dd8b6dd701abd735dae6b
Guilherme Rocha <GuiPython2001@gmail.com>
2021-08-23 13:53:43 -0300

    add enums categoria de séries, entidades e querys(SQL)

---
+++ Enums/Category.cs
@@ -0,0 +1,11 @@
+
+namespace Enums
+{
+   public enum Category{
+        TERROR = 1,
+        SUSPENCE = 2,
+        DRAMA = 3,
+        AVENTURA =4,
+        ACAO=5
+   } 
+}
---
+++ Enums/Query.cs
@@ -0,0 +1,11 @@
+
+namespace Enums{
+    public enum Query{
+        
+        INSERT = 1,
+        SELECT = 2,
+        UPDATE = 3,
+        DELETE = 4
+
+    }
+}
